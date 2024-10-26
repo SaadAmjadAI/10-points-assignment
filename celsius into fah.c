514d91733d36261a0c35df89f93545c5efd53558
SaadAmjadAI <saadamjadai@gmail.com>
2024-10-26 22:32:52 +0500

    Add files via upload

---
+++ celsius into fah.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+    float c,f;
+    printf("put the temperature in celcius:");
+    scanf("%f",&c);
+    f=(c+32 )*5/9;
+    printf("\n temperature in fahreinheit : %f",f);
+    getch();
+
+    
+    
+}
